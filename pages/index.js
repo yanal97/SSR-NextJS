8f4a76d270cdd45a22695bf14609e9a0a1a946dc
yanal otaibi <yanalotaibi@yanals-MacBook-Pro.local>
2021-03-05 23:34:14 +0200

    init2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
+import fs from "fs";
+import path from "path";
+import Link from "next/link";
+
 function HomePage(props) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {props.products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
